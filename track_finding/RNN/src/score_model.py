49245499acd2317adcfa22c6994551825c09f689
Manolis Kargiantoulakis <ekargian@fnal.gov>
2022-02-08 11:52:57 -0600

    updates on the model notebook and scoring script

--- track_finding/RNN/src/score_model.py
+++ track_finding/RNN/src/score_model.py
@@ -16,9 +16,11 @@ import keras
 
 import argparse
 parser = argparse.ArgumentParser(description=__doc__, epilog=' ', formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('--modelname',default='test',help='model files expected in format [modelname]_<seed-location>.h5 in the saved_models dir')
+parser.add_argument('--modelname',default='test',help='Model files expected in format [modelname]_<seed-location>.h5 in the saved_models dir.')
+parser.add_argument('--datafile',default=None,help='Dataset file to score on. Use default if None. pkl file expected.')
 args = parser.parse_args()
 modelname=args.modelname
+datafile=args.datafile
 
 ## Build the NN
 tracker=detector.build_tracker()
@@ -32,18 +34,16 @@ for seed_location in ['front','middle','back']:
 
 ## Read in synthetic events
 import pickle
-ml_tracking_dir=os.environ['ML_TRACKING']
-synthetic_dir=ml_tracking_dir+'/synthetic_data/pkl/'
-rnn_dir=ml_tracking_dir+'/track_finding/RNN'
-events_file=open(synthetic_dir+'synthetic_events.pkl','rb')
+if datafile is None:
+    ml_tracking_dir=os.environ['ML_TRACKING']
+    synthetic_dir=ml_tracking_dir+'/synthetic_data/pkl/'
+    ## read in the 2-track synthetic events for scoring
+    events_file=open(synthetic_dir+'synthetic_events.pkl','rb')
+else: 
+    events_file=open(datafile,'rb')
 evts_hits,evts_ids = pickle.load(events_file,encoding='latin1')
-if 'synthetic' in synthetic_dir:
-    print('\n Reading in %d synthetic events' % evts_hits.shape[0])
-## read in the 2-track synthetic events for scoring
-events_file=open(synthetic_dir+'synthetic_events.pkl','rb')
-evts_hits_2track,evts_ids_2track = pickle.load(events_file,encoding='latin1')
-evts_hits_test=evts_hits_2track[:1000]
-evts_ids_test=evts_ids_2track[:1000]
+evts_hits_test=evts_hits[:1000]
+evts_ids_test=evts_ids[:1000]
     
 ### Get accuracy metrics
 ## First the raw selection score
@@ -56,6 +56,9 @@ print('Time for raw scoring (s) : ', raw_score_time)
 ## Then score for accuracy/error after seed merging
 start_time=time.perf_counter()
 predicted_ids=seed_merging.predict_events(tracker_NN, evts_hits_test)#,verbose=1)
+end_time=time.perf_counter()
+predict_time=end_time-start_time
+start_time=time.perf_counter()
 full_score=util.score_function(evts_ids_test, predicted_ids )
 end_time=time.perf_counter()
 full_score_time=end_time-start_time
@@ -63,4 +66,5 @@ n_tracks=0
 for predicted_id in predicted_ids:
     n_tracks += np.max(predicted_id)
 print('Total tracks found: ', n_tracks)
+print('Time for prediction (s) : ', prediction_time)
 print('Time for full scoring (s) : ', full_score_time)
